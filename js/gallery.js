d40292c357e9e4e77004ba5a46cb99d12ac86c9f
amoeller-cs <amoeller.psy@gmail.com>
2020-10-04 21:48:18 -0700

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,13 @@
 let imageDex = 0;
 let images = ["imgs/me.jpg", "imgs/appleADay.png", "imgs/erDiagram.png"];
+let alts = ["It's me!.", "Calendar page from my Android Application, Apple A Day.", "An ER diagram from my stock management relational database model."];
 let totalImages = 3;
 
 function nextImage() {
     imageDex++;
     imageDex = imageDex % totalImages;
     document.getElementById("photo").src = images[imageDex];
+    document.getElementById("photo").alt = alts[imageDex];
 }
 
 const nextButton = document.querySelector("#next");
