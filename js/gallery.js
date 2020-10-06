d468560fed55b2d1a27582217aa2e8cd71c39eb8
amoeller-cs <amoeller.psy@gmail.com>
2020-10-05 17:07:44 -0700

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -1,24 +1,24 @@
 let imageDex = 0;
 let images = [
-    "imgs/appleADayHome.png",
-    "imgs/appleADayCal.png",
-    "imgs/appleADaySymp.png",
-    "imgs/erDiagram.png",
+  "imgs/appleADayHome.png",
+  "imgs/appleADayCal.png",
+  "imgs/appleADaySymp.png",
+  "imgs/erDiagram.png",
 ];
 let alts = [
-    "Home page from my Android Application, Apple A Day",
-    "Calendar page from Apple A Day, colors on Calendar indicate symptom severity",
-    "U/I for the symptom selection page from Apple A Day",
-    "An ER diagram from my stock management relational database model",
+  "Home page from my Android Application, Apple A Day",
+  "Calendar page from Apple A Day, colors on Calendar indicate symptom severity",
+  "U/I for the symptom selection page from Apple A Day",
+  "An ER diagram from my stock management relational database model",
 ];
 let totalImages = 4;
 
 function nextImage() {
-    imageDex++;
-    imageDex = imageDex % totalImages;
-    document.getElementById("photo").src = images[imageDex];
-    document.getElementById("photo").alt = alts[imageDex];
-    document.getElementById("descript").innerHTML = alts[imageDex];
+  imageDex++;
+  imageDex = imageDex % totalImages;
+  document.getElementById("photo").src = images[imageDex];
+  document.getElementById("photo").alt = alts[imageDex];
+  document.getElementById("descript").innerHTML = alts[imageDex];
 }
 
 const nextButton = document.querySelector("#next");
--- js/webJS.js
+++ js/webJS.js
@@ -1,6 +1,6 @@
 function darkMode() {
-    let siteHTML = document.body;
-    siteHTML.classList.toggle("dark-mode");
+  let siteHTML = document.body;
+  siteHTML.classList.toggle("dark-mode");
 }
 
 const darkModeButton = document.querySelector("#darkMode");
